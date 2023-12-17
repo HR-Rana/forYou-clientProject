d9f141da5acc1bfb660793a7fca82acf0ac73abe
webworld420 <76555387+webworld420@users.noreply.github.com>
2023-12-17 15:32:51 +0600

    auth

--- For-You/src/pages/AuthPage/Login.jsx
+++ For-You/src/pages/AuthPage/Login.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Login() {
-  return (
-    <div>Login</div>
-  )
+	return (
+		<div className="login-page">
+			<h3>Login your Account</h3>
+			<div className="login-section">
+				<form action="">
+					<input type="email" placeholder="enter your email" />
+					<input type="password" placeholder="enter your Password" />
+					<input type="submit" value="Login" />
+          <div className="login-reg-refer">
+            <p>Don't have any account?</p>
+            <p><Link to={'/Registration'}>Registration Now</Link></p>
+          </div>
+				</form>
+				<div className="Login-otherWay flex">
+          <p>or Try another way</p>
+					<div className="google">
+						<button>Google</button>
+					</div>
+          <div className="facebook">
+            <button>Facebook</button>
+          </div>
+				</div>
+			</div>
+		</div>
+	);
 }
--- For-You/src/pages/AuthPage/Registration.jsx
+++ For-You/src/pages/AuthPage/Registration.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Registration() {
-  return (
-    <div>Registration</div>
-  )
+	return (
+		<div className="Registration-page login-page">
+			<h3>Registration Now</h3>
+			<div className="login-section">
+				<form action="">
+					<input type="text" placeholder="Enter your Name" />
+					<input type="email" placeholder="enter your email" />
+					<input type="password" placeholder="Enter your Password" />
+					<input type="password" placeholder="Confirm Password" />
+					<input type="submit" value="Registration" />
+					<div className="login-reg-refer">
+						<p>Do you have account?</p>
+						<p>
+							<Link to={"/Registration"}>Login Now</Link>
+						</p>
+					</div>
+				</form>
+				<div className="Login-otherWay flex">
+					<p>or Try another way</p>
+					<div className="google">
+						<button>Google</button>
+					</div>
+					<div className="facebook">
+						<button>Facebook</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
