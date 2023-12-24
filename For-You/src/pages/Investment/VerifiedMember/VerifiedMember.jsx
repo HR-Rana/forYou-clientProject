9270b09e9ea6e4a24ad9311b3abc6fd59c81432b
webworld420 <76555387+webworld420@users.noreply.github.com>
2023-12-24 23:33:41 +0600

    push

--- For-You/src/pages/Investment/VerifiedMember/VerifiedMember.jsx
+++ For-You/src/pages/Investment/VerifiedMember/VerifiedMember.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function VerifiedMember() {
   return (
-    <div>VerifiedMember</div>
+    <div className='verified-member-page'>
+        <div className="page-title">
+            <h3>Our Verified Users...</h3>
+         </div>
+         <section>
+            <div className="container">
+                
+            </div>
+         </section>
+    </div>
   )
 }
---
+++ For-You/src/pages/Investment/VerifiedMember/VerifiedUserList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function VerifiedUserList() {
+  return (
+    <div className='verified-user-card'>
+        
+    </div>
+  )
+}
