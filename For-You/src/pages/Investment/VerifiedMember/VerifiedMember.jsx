43598608aa06ddd20e13531b900638edf6d40636
webworld420 <76555387+webworld420@users.noreply.github.com>
2023-12-25 00:00:00 +0600

    text

--- For-You/src/pages/Investment/VerifiedMember/VerifiedMember.jsx
+++ For-You/src/pages/Investment/VerifiedMember/VerifiedMember.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
 import VerifiedUserList from "./VerifiedUserList";
 
 export default function VerifiedMember() {
@@ -8,9 +10,14 @@ export default function VerifiedMember() {
 				<h3>Our Verified Users...</h3>
 			</div>
 			<section>
-				<div className="container-fluid transaction-short-view">
+				<div className="container-fluid verified-Users transaction-short-view">
 					<div className="transaction">
 						<VerifiedUserList />
+						<div className="pagination">
+							<Stack spacing={2}>
+								<Pagination count={10} color="primary" />
+							</Stack>
+						</div>
 					</div>
 				</div>
 			</section>
