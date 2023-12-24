cef1bb6fbf85a33d754cddf668f2affde2a12598
webworld420 <76555387+webworld420@users.noreply.github.com>
2023-12-24 23:40:41 +0600

    verified user table

--- For-You/src/pages/Investment/VerifiedMember/VerifiedUserList.jsx
+++ For-You/src/pages/Investment/VerifiedMember/VerifiedUserList.jsx
@@ -1,9 +1,226 @@
-import React from 'react'
+import React from "react";
 
 export default function VerifiedUserList() {
-  return (
-    <div className='verified-user-card'>
-        
-    </div>
-  )
+	return (
+		<div className="verified-user-card">
+			<table>
+				<thead>
+					<th>Name</th>
+					<th>Date</th>
+					<th>Deposit Amount</th>
+					<th>Currency</th>
+				</thead>
+				<tr>
+					<td>
+						<div className="flex name">
+							<span>
+								<img src={img1} alt=" " />
+							</span>
+							<h5> Jim Adams</h5>
+						</div>
+					</td>
+					<td>
+						<div className="date">
+							<h5> 31-11-2023</h5>
+						</div>
+					</td>
+					<td>
+						<h5> $350</h5>
+					</td>
+					<td>
+						<h5>$ Dolar</h5>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div className="flex name">
+							<span>
+								<img src={img2} alt=" " />
+							</span>
+							<h5> Jim Adams</h5>
+						</div>
+					</td>
+					<td>
+						<div className="date">
+							<h5> 04-11-2023</h5>
+						</div>
+					</td>
+					<td>
+						<h5> $350</h5>
+					</td>
+					<td>
+						<h5>$ Dolar</h5>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div className="flex name">
+							<span>
+								<img src={img3} alt=" " />
+							</span>
+							<h5> Jim Adams</h5>
+						</div>
+					</td>
+					<td>
+						<div className="date">
+							<h5> 02-12-2023</h5>
+						</div>
+					</td>
+					<td>
+						<h5> $350</h5>
+					</td>
+					<td>
+						<h5>$ Dolar</h5>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div className="flex name">
+							<span>
+								<img src={img4} alt=" " />
+							</span>
+							<h5> Jim Adams</h5>
+						</div>
+					</td>
+					<td>
+						<div className="date">
+							<h5> 06-12-2023</h5>
+						</div>
+					</td>
+					<td>
+						<h5> $350</h5>
+					</td>
+					<td>
+						<h5>$ Dolar</h5>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div className="flex name">
+							<span>
+								<img src={img5} alt=" " />
+							</span>
+							<h5> Jim Adams</h5>
+						</div>
+					</td>
+					<td>
+						<div className="date">
+							<h5> 06-12-2023</h5>
+						</div>
+					</td>
+					<td>
+						<h5> $350</h5>
+					</td>
+					<td>
+						<h5>$ Dolar</h5>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div className="flex name">
+							<span>
+								<img src={img6} alt=" " />
+							</span>
+							<h5> Jim Adams</h5>
+						</div>
+					</td>
+					<td>
+						<div className="date">
+							<h5> 06-12-2023</h5>
+						</div>
+					</td>
+					<td>
+						<h5> $350</h5>
+					</td>
+					<td>
+						<h5>$ Dolar</h5>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div className="flex name">
+							<span>
+								<img src={img7} alt=" " />
+							</span>
+							<h5> Jim Adams</h5>
+						</div>
+					</td>
+					<td>
+						<div className="date">
+							<h5> 06-12-2023</h5>
+						</div>
+					</td>
+					<td>
+						<h5> $350</h5>
+					</td>
+					<td>
+						<h5>$ Dolar</h5>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div className="flex name">
+							<span>
+								<img src={img8} alt=" " />
+							</span>
+							<h5> Jim Adams</h5>
+						</div>
+					</td>
+					<td>
+						<div className="date">
+							<h5> 06-12-2023</h5>
+						</div>
+					</td>
+					<td>
+						<h5> $350</h5>
+					</td>
+					<td>
+						<h5>$ Dolar</h5>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div className="flex name">
+							<span>
+								<img src={img9} alt=" " />
+							</span>
+							<h5> Jim Adams</h5>
+						</div>
+					</td>
+					<td>
+						<div className="date">
+							<h5> 06-12-2023</h5>
+						</div>
+					</td>
+					<td>
+						<h5> $350</h5>
+					</td>
+					<td>
+						<h5>$ Dolar</h5>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div className="flex name">
+							<span>
+								<img src={img10} alt=" " />
+							</span>
+							<h5> Jim Adams</h5>
+						</div>
+					</td>
+					<td>
+						<div className="date">
+							<h5> 06-12-2023</h5>
+						</div>
+					</td>
+					<td>
+						<h5> $350</h5>
+					</td>
+					<td>
+						<h5>$ Dolar</h5>
+					</td>
+				</tr>
+			</table>
+		</div>
+	);
 }
